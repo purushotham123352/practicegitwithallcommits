381984549392532b3a846e4299ce918edf373530
vinodam.purushotham <vinodampurushotham@gmail.com>
2025-08-19 15:22:18 +0530

    Added new controller class

--- gitpractice/src/main/java/com/example/gitpractice/GitpracticeApplication.java
+++ gitpractice/src/main/java/com/example/gitpractice/GitpracticeApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+
 public class GitpracticeApplication {
 
-	public String get(){
-		return "Hello .....";
-	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitpracticeApplication.class, args);
